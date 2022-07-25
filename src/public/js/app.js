60aeed401567037f2fe987ca0b781e626aba4eed
csound93 <csound93@gmail.com>
2022-07-25 18:12:10 +0900

    > stun 추가 테스트 중

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,6 +13,8 @@ let myStream;
 let muted = false;
 let cameraOff = false;
 let roomName;
+/** @type {RTCPeerConnection} */
+let myPeerConnection;
 
 async function getCameras() {
   try {
@@ -35,7 +37,7 @@ async function getCameras() {
 
 async function getMedia(deviceId) {
   const initConstraints = { audio: true, video: { facingMode: "user" } }; //browser에서 정한 video로 설정
-  const cameraConstraints = { audio: true, video: { deviceId: deviceId } }; //browser에서 정한 video로 설정
+  const cameraConstraints = { audio: true, video: { deviceId: deviceId } }; //deviceId가 맞는 video로 설정
 
   try {
     myStream = await navigator.mediaDevices.getUserMedia(deviceId ? cameraConstraints : initConstraints);
@@ -73,8 +75,15 @@ function handleCameraClick() {
   }
 }
 
-function handleCameraChange() {
-  getMedia(camerasSelect.value);
+async function handleCameraChange() {
+  await getMedia(camerasSelect.value);
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders() //
+      .find((sender) => sender.track.kind === "video");
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
@@ -84,18 +93,20 @@ camerasSelect.addEventListener("input", handleCameraChange);
 // Welcome Form (join a room)
 
 const welcome = document.getElementById("welcome");
-const welcomeForm = welcome.querySelector("#welcome form");
+const welcomeForm = welcome.querySelector("form");
 
-function startMedia() {
+async function initCall() {
   welcome.hidden = true;
   call.hidden = false;
-  getMedia();
+  await getMedia();
+  makeConnection();
 }
 
-function handleWelcomeSubmit(event) {
+async function handleWelcomeSubmit(event) {
   event.preventDefault();
   const input = welcomeForm.querySelector("input");
-  socket.emit("join_room", input.value, startMedia);
+  await initCall();
+  socket.emit("join_room", input.value);
   roomName = input.value;
   input.value = "";
 }
@@ -104,6 +115,64 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
 // Socket Code
 
-socket.on("welcome", () => {
-  console.log("someone join");
+socket.on("welcome", async () => {
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("sent the offer");
+  socket.emit("offer", offer, roomName);
 });
+
+socket.on("offer", async (offer) => {
+  console.log("received the offer");
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit("answer", answer, roomName);
+  console.log("sent the answer");
+});
+
+socket.on("answer", (answer) => {
+  console.log("received the answer");
+  myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on("ice", (ice) => {
+  console.log("receive candidate");
+  myPeerConnection.addIceCandidate(ice);
+});
+
+// RTC Code
+
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          "stun:stun.l.google.com:19302", //
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+          "stun:stun3.l.google.com:19302",
+          "stun:stun4.l.google.com:19302",
+        ],
+      },
+    ],
+  });
+  myPeerConnection.addEventListener("icecandidate", handleIce);
+  myPeerConnection.addEventListener("addstream", handleAddStream);
+  myStream
+    .getTracks() //
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
+
+function handleIce(/** @type {RTCPeerConnectionIceEvent} */ data) {
+  console.log("sent candidate");
+  socket.emit("ice", data.candidate, roomName);
+}
+
+function handleAddStream(data) {
+  const peersFace = document.getElementById("peersFace");
+  console.log("got an stream from my peer");
+  console.log("My steam", myStream);
+  console.log("Peer's stream", data.stream);
+  peersFace.srcObject = data.stream;
+}
